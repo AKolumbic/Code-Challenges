41659d4e22ac8d331161bea81271eb6549e93fd2
Andrew Kolumbic <spguy13jamc@sbcglobal.net>
2018-10-11 11:22:00 -0700

    adds comments for logic in reverseCase

--- Lambda/String-Challenges/reverseCasing.js
+++ Lambda/String-Challenges/reverseCasing.js
@@ -1,14 +1,21 @@
 // Write a function that takes in a string, reverses the 'casing' of that string.
 
 function reverseCase(str) {
+    // we split the input string into an array of individual characters("letter") and then
+    // map over each "letter" since we will be changing them. All of the logic is put onto
+    // a variable aptly named "result"
     let result = str.split("").map((letter)=> {
-      if (letter.toLowerCase() === letter) {
-        return letter.toUpperCase();
-      } else {
-        return letter.toLowerCase();
-      }
+        // make all letters lowercase and check if they are already lowercase
+        if (letter.toLowerCase() === letter) {
+            // if the letter was originally lowercase, make it uppercase
+            return letter.toUpperCase();
+        } else {
+            // If it was originally uppercase, make it lowercase
+            return letter.toLowerCase();
+        }
     })
+    // return the result, joined back together since it became an array in the logic above.
     return result.join("");
-    }
+}
     
 reverseCase('HelloWorld');
