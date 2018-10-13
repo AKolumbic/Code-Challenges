ce5a2ccd17bf6a020e22ff23f9ecc1233006865a
Andrew Kolumbic <spguy13jamc@sbcglobal.net>
2018-10-13 12:34:18 -0700

    adds reverseNumber logic

--- Lambda/Integer-Challenges/reverseNumber.js
+++ Lambda/Integer-Challenges/reverseNumber.js
@@ -6,15 +6,26 @@ example input: 12345 / example output: 54321
 */
 
 function reverseNumber(n) {
-    let num = n;
-    let reversedNum = 0;
+    let reversed = 0;
 
-    while(num !==0) {
-        reversedNum *= 10;
-        reversedNum += num % 10;
-        num -= num % 10;
-        num /= 10;
+    while(n !==0) {
+        reversed*= 10;
+        reversed+= n % 10;
+        n -= n % 10;
+        n /= 10;
     }
 
-    return reversedNum;
+    return reversed;
 }
+
+function forloop_reverseNumber(n) {
+    for (let reversed=0; n; n = Math.floor(n / 10)) {
+        reversed *= 10;
+        reversed += n % 10;
+    }
+
+    return reversed;
+}
+
+console.log(reverseNumber(12345));
+console.log(forloop_reverseNumber(12345));
