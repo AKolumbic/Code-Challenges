1f286f3ee2e57e652b141f3e09b46d1fae9c0f79
Andrew Kolumbic <spguy13jamc@sbcglobal.net>
2018-10-23 11:20:24 -0700

    adds working solution
    -works with valid input, as per instructions
    -has comments and console.logs

--- Code-Wars/strayNumber.js
+++ Code-Wars/strayNumber.js
@@ -1,18 +1,13 @@
 function stray(numbers) {
-    const firstNum = numbers[0];
-
-    for (let i=0; i<numbers.length; i++) {
-        // the thinking behind this IF statement is that if the first number is the stray
-        // then then it will match the iteration, and the 2nd and 3rd iteration will match. 
-        // Unfortunetly this only solves that particular edge case
-        if (numbers[i] == firstNum && numbers[i+1] == numbers[i+2]) {
-            return numbers[i];
-        } 
-        // else if (numbers[i] != firstNum) {
-        //     return numbers[i];
-        // } 
-    }
+    console.log("Input: ", numbers);
+    // sort the input into an array
+    var sortedNums = numbers.sort();
+    console.log("Sorted Input: ", sortedNums)
+    
+    // return the last number of the sorted array
+    console.log("Last Number: ", sortedNums[sortedNums.length-1])
+    return sortedNums[sortedNums.length-1]
 }
 
-// console.log(stray([17, 17, 17, 17, 3, 17, 17])) // returns 3
-console.log(stray([8, 1, 1, 1, 1, 1 ])) // returns 3
+console.log(stray([17, 17, 17, 17, 3, 17, 17])) // returns 3
+console.log(stray([8, 1, 1, 1, 1, 1 ])) // returns 8
