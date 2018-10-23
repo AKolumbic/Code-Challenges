d8b8f1e22db4d99a3abf2153898f10b076510244
Andrew Kolumbic <spguy13jamc@sbcglobal.net>
2018-10-23 10:57:53 -0700

    new if solves edge case where 1st integer is the stray
    -however, it breaks the normal functionality
    -NOT a solution

--- Code-Wars/strayNumber.js
+++ Code-Wars/strayNumber.js
@@ -2,10 +2,13 @@ function stray(numbers) {
     const firstNum = numbers[0];
 
     for (let i=0; i<numbers.length; i++) {
-        if (numbers[i] != firstNum) {
+        if (numbers[i] == firstNum && numbers[i+1] == numbers[i+2]) {
             return numbers[i];
-        }
+        } else if (numbers[i] != firstNum) {
+            return numbers[i];
+        } 
     }
 }
 
-console.log(stray([8, 1, 1, 1, 1, 1 ])) // should return 3
+console.log(stray([17, 17, 17, 17, 3, 17, 17])) // returns 3
+console.log(stray([8, 1, 1, 1, 1, 1 ])) // returns 3
