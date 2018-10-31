4bc8d5ac5d72fa03ae6c0d3f0da8abe37c90e5c5
Andrew Kolumbic <spguy13jamc@sbcglobal.net>
2018-10-31 14:06:19 -0700

    removes original version

--- Lambda/Number-Challenges/romanNumeralize.js
+++ Lambda/Number-Challenges/romanNumeralize.js
@@ -2,32 +2,6 @@
 Define a function that takes in a positive integer and returns the Roman Numeral representation of that number.  
 */
 
-// function romanNumeralize(num) {
-//     var roman ="";
-  
-//     var values = [1000,900,500,400,100,90,50,40,10,9,5,4,1];
-//     var literals = ["M","CM","D","CD","C","XC","L","XL","X","IX","V","IV","I"];
-  
-  
-//     for(i=0;i<values.length;i++){
-//         console.log(values[i])
-//         console.log(num)
-//         console.log(roman)
-//       if(num>=values[i]){
-//         if(5<=num && num<=8) num -= 5;
-//         else if(1<=num && num<=3) num -= 1;
-//         else num -= values[i];
-//         roman += literals[i];
-//         i--;
-//       }
-//     }
-
-//    return roman;
-// }
-  
-  
-// console.log(romanNumeralize(1874));
-
 function romanNumeralize(n) {
     let number = n;
     const rome = {
