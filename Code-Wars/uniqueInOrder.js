d1fffd414a0cb68de912631c2e9788049105e071
Andrew Kolumbic <spguy13jamc@sbcglobal.net>
2018-11-03 12:02:19 -0700

    adds attack plan for function and bad WIP I was working on

--- Code-Wars/uniqueInOrder.js
+++ Code-Wars/uniqueInOrder.js
@@ -10,18 +10,41 @@ uniqueInOrder('AAAABBBCCDAABBB') == ['A', 'B', 'C', 'D', 'A', 'B']
 uniqueInOrder('ABBCcAD')         == ['A', 'B', 'C', 'c', 'A', 'D']
 uniqueInOrder([1,2,2,3,3])       == [1,2,3]
 
+
+STEPS/PLAN:
+1. Check if input is array or string.
+    * If String:
+    * Split the input into an array.
+
+2. init a cache with the first value in the split input.(call this output)
+3. loop over input
+    * if input interation is not in output, push it into output.
+
+4. return output.
+
 */
 
 function uniqueInOrder(input) {
-    var output = [];
+    // if (input = String) {
+        const spliced = input.split('');
+        console.log(spliced[0]);
+        const output = [spliced[0]];
 
-    if (input === String) {
+        for(let i=0; i<spliced.length; i++) {
+            for(let j=0; j<spliced.length; j++) {
+                // console.log("I", input[i]);
+                // console.log("J", input[j]);
+            }
+        }
 
-    }
 
-    if (input === Array) {
-
-    }
-
-    return output;
+        return output;
+    // }
+    // } else if (input = Array) {
+    //     console.log("You're seeing this because input is an array");
+    // }
 }
+
+
+console.log(uniqueInOrder('AAAABBBCCDAABBB'))
+// console.log(uniqueInOrder([1,2,2,3,3]) )
