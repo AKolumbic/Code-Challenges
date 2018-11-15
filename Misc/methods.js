62d84963a7c630ccaaa1cb2f80fbd0c957ad6a88
Andrew Kolumbic <spguy13jamc@sbcglobal.net>
2018-11-14 22:43:52 -0800

    adds basic methods in new MISC dir

---
+++ Misc/methods.js
@@ -0,0 +1,32 @@
+/*
+Basic Command Line Methods
+*/
+
+
+let input = process.argv.slice();
+
+let operation = input[2];
+
+let a = input[3];
+let b = input[4];
+
+let methods = {
+    add: function(x, y) {
+        return Number(x) + Number(y);
+    },
+    subtract: function(x, y) {
+        return Number(x) - Number(y);
+    },
+    multiply: function(x, y) {
+        return Number(x) * Number(y);
+    },
+    divide: function(x, y) {
+        return Number(x) / Number(y);
+    }
+}
+
+if ((!methods[operation]) || (a||x == NaN)) {
+    console.log("Invalid Input")
+}
+
+console.log(methods[operation](a,b));
