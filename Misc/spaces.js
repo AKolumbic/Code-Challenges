1735b49b9fa9439d81d0c7d180df6b53bd7a4365
Andrew Kolumbic <spguy13jamc@sbcglobal.net>
2018-12-04 12:18:12 -0800

    adds replaceSpaces function

---
+++ Misc/spaces.js
@@ -0,0 +1,13 @@
+/*
+
+This is my own personal challange to myself for a project. For search functionality,
+I need to use regex to replace spaces (' ') with ('$'). Should be easy enough.
+
+*/
+
+function replaceSpaces(input) {
+    return input.toString().replace(/\s/g, '%')
+};
+
+console.log(replaceSpaces("Dr. Boom"));
+console.log(replaceSpaces("Curse of Naxrammas"));
